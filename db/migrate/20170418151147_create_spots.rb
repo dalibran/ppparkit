c2eb27865f8e98e44154a7acad531842b9a3d4c3
Mengzhou Li <mengzhou.li@gmail.com>
2017-04-18 14:44:40 -0400

    seed has users and spots now need parkits

--- db/migrate/20170418151147_create_spots.rb
+++ db/migrate/20170418151147_create_spots.rb
@@ -1,12 +1,12 @@
 class CreateSpots < ActiveRecord::Migration[5.0]
   def change
     create_table :spots do |t|
-      t.string :sNoPlace
-      t.float :lat
-      t.float :lng
+      t.string :place_no
+      t.decimal :lat, :precision => 17, :scale => 15
+      t.decimal :lng, :precision => 17, :scale => 15
       t.string :kind
-      t.boolean :bike_head
-      t.string :payType
+      t.integer :bike_head
+      t.string :pay_type
       t.integer :hour_rate
       t.integer :max_rate
       t.string :status
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,68 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#   Character.create(name: 'Luke', movie: movies.first) 
+require 'csv'
+Spot.destroy_all
+ParkIt.destroy_all
+User.destroy_all
+puts "done throwing everything in the trash can"
+
+emails = [["mengzhou.li@gmail.com", "meng"],
+	["mengzhouli@gmail.com", "mz"],
+	["vonture@gmail.com", "vonture"],
+	["geoff@vonture.net", "geoff"],
+	["ashraf.caspian@gmail.com", "ashraf"],
+	["d.alibran@gmail.com", "dana"],
+	["dana.alibran@gmail.com", "dan"],
+	["dalibran@gmail.com", "dana69"],
+	["montreal@lewagon.org", "mtl"],
+	["mg.ayoub@lewagon.org", "mg"],
+	["antoine.ayoub@lewagon.org", "antoine"]]
+
+
+emails.each do |sub|
+	User.create!(email: sub[0], password: "pppassword", password_confirmation: "pppassword", username: sub[1] )
+end
+
+byebug
+
+filepath    = 'db/places.csv'
+csv_options = { col_sep: ',', headers: true, row_sep: :auto, encoding:'iso-8859-1:utf-8' }
+CSV.foreach(filepath, csv_options) do |row|
+	pay = ""
+
+	if row["sTypeExploitation"].nil?
+		pay = "reg" 
+	elsif row["sTypeExploitation"].include? "Parcojour"
+		pay = "day"
+	else
+		pay ="reg"
+	end
+
+	rate = ""
+	if row["nTarifMax"].blank?
+		rate = nil
+	else
+		rate = row["nTarifMax"].to_i
+	end
+
+	avail = ""
+	if row["nSupVelo"].to_i.zero?
+		avail = "none"
+	else
+		avail = "available"
+	end
+
+	Spot.create!(place_no: row["sNoPlace"],
+		lat: row["nPositionCentreLatitude"],
+		lng: row["nPositionCentreLongitude"],
+		kind: row["sGenre"],
+		bike_head: row["nSupVelo"].to_i,
+		pay_type: pay,
+		hour_rate: row["nTarifHoraire"].to_i,
+		max_rate: rate,
+		status: "taken",
+		bike_head_status: avail
+		)
+end
