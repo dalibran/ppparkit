3bbfbc179c5a00966242996ae18aecafe8843e13
Mengzhou Li <mengzhou.li@gmail.com>
2017-04-19 11:57:49 -0400

    new parkit controller with skelton of new and create (no forms

--- app/models/park_it.rb
+++ app/models/park_it.rb
@@ -2,5 +2,5 @@ class ParkIt < ApplicationRecord
   belongs_to :spot
   belongs_to :user
 
-  validates :kind, inclusion: { in: ["Take a Spot", "Leave a Spot", "Leave a Spot with Time Remaining"] }
+  validates :kind, inclusion: { in: ["park", "leave", "see"] }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  resources :spots
+  resources :spots, only: [:index, :show] do
+  	resources :park_its, only: [:new, :create]
+  end
+
+  resources :park_its, only: [:edit, :update]
 
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
