03db02cc6a2a712903b82dd83cca05fc1d972b10
Mengzhou Li <mengzhou.li@gmail.com>
2017-04-27 11:44:01 -0400

    added flash messages telling people to zoom out if htere is not enough parking

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,13 +10,17 @@ class UsersController < ApplicationController
 
 		@park_it = ParkIt.new
 		@spots = Spot.near(current_user.position, 0.2)
-		if @spots.size < 2
-			flash[:notice] = "Are you sure you are in Montreal?"
-		elsif @spots.size < 50
+
+		if @spots.size < 50
+		flash[:notice] = "Not much parking nearby, zoom out to see more."
 	      @spots = Spot.near(current_user.position, 0.40)
 	    elsif @spots.size > 200
 	      @spots = Spot.near(current_user.position, 0.15)
 	    end
+
+	    if @spots.size < 2
+			flash[:notice] = "Are you sure you are in Montreal?"
+		end
 		get_markers(@spots, @park_it)
 
 		respond_to do |format|
