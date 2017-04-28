d346c840225d0646af0829c04cd57f7d2338b68f
Dana Alibrandi <dalibrandi@gmail.com>
2017-04-28 14:55:54 -0400

    remove paid_until validations

--- app/models/park_it.rb
+++ app/models/park_it.rb
@@ -2,6 +2,5 @@ class ParkIt < ApplicationRecord
   belongs_to :spot
   belongs_to :user
 
-  validates :paid_until, presence: true, allow_nil: false
   # validates :kind, inclusion: { in: ["park", "leave", "see", "update"] }
 end
